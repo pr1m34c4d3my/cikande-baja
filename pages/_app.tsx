cdb85a630c18f41679091df01ae03fb44cf1af78
pr1m34c4d3my <akunbaruwisnu@gmail.com>
2023-01-05 21:26:56 +0700

    add loading transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,49 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function Loading(): any {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url: string) =>
+      url !== router.asPath && setLoading(true);
+    const handleComplete = (url: string) =>
+      url === router.asPath &&
+      setTimeout(() => {
+        setLoading(false);
+      }, 200);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  });
+
+  return (
+    loading && (
+      <div className="spinner-wrapper">
+        <div className="spinner"></div>
+      </div>
+    )
+  );
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Loading />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
