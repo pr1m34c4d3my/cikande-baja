ce061f742f9993a5e51f81390f43973e0eec6383
Wisnu Budi Laksono <60870047+pr1m34c4d3my@users.noreply.github.com>
2023-01-12 10:19:30 +0700

    Update [slug].tsx

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -157,7 +157,11 @@ const ProductPost = ({ categories, product, products }: any) => {
           content={`Produk ${product.productTitle} dari Cikande Indobaja Mandiri`}
         />
         <meta property="og:description" content={product.productDescription} />
+        <meta property="og:url" content={URL} />
         <meta property="og:image" content={product.productPhoto1.url} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
+        <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
